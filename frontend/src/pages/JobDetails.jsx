b964248aa3b286dad20ff5396c064ffeccf60e76
yash825-bit <goyal.yash18335@gmail.com>
2025-11-09 18:09:32 +0530

    basic frontend

---
+++ frontend/src/pages/JobDetails.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function JobDetails() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-2xl font-bold">{"JobDetails"}</h1>
+    </div>
+  );
+}
